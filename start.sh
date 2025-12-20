2a88d205f43dbf9a8f1b62ed5b1e12b47c557229
kleber Lima <klebelim@gmail.com>
2025-12-20 09:27:37 -0300

    Fix Dockerfile content

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-gunicorn main:app
+# Instala aiohttp explicitamente para contornar problemas de cache/dependência do Render
+pip install aiohttp
+
+# Inicia a aplicação
+gunicorn app:app
 
